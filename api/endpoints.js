fc315ee77bf5e2b3082180dbda333088ae65c255
Alexandra-Iuga <100163768+Alexandra-Iuga@users.noreply.github.com>
2022-08-09 14:20:23 +0300

    updated

--- api/endpoints.js
+++ api/endpoints.js
@@ -1,4 +1,4 @@
-const API_KEY = "ea3e3338d6a2f4fe6d246649169f3479";
+const API_KEY = "e9397ee9f925b30afe098d44534ab069";
 
 function getCurrentWeatherEndpoint(city) {
   return `https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=ro&units=metric&appid=${API_KEY}`;
