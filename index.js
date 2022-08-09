668f2a59f61087e99dcb6738112193872726a60d
Alexandra-Iuga <100163768+Alexandra-Iuga@users.noreply.github.com>
2022-08-09 14:23:04 +0300

    updated

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
 const currentCityTag = document.querySelector(".current-city");
 let currentCity = localStorage.getItem("city");
 
-// Daca nu avem oras salvat in localStorage, salvam orasul default, adica Bucuresti.
 if (!currentCity) {
   localStorage.setItem("city", "București");
   currentCity = "București";
 }
 
-// Actualizam orasul afisat pe ecran.
 currentCityTag.innerHTML = currentCity;
 
-// Afisam vremea curenta si predictia pe 5 zile.
 displayCurrentWeather(currentCity);
 displayWeatherForecast(currentCity);
 
-// Scroll to top button
 let scrollToTopButton = document.querySelector(".scroll-to-top");
 
 function handleScroll() {
